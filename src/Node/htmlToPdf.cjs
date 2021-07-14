942c34e7981cc7c82daf34efb3026b7c664f33e7
Peder Arne Godø <pegod@equinor.com>
2021-07-14 11:36:07 +0200

    moving to use require

--- src/Node/htmlToPdf.cjs
+++ src/Node/htmlToPdf.cjs
@@ -1,10 +1,12 @@
-﻿module.exports = async function(result, html, config) {
-    import puppeteer from "puppeteer";
-    import tmp  from "tmp";
-    import fs from "fs";
-    import fileUrl from "file-url";
-    import PDFMerge from "pdf-merge";
-    import path from "path";
+﻿const puppeteer = require("puppeteer");
+const tmp = require("tmp");
+const fs = require("fs");
+const fileUrl = require("file-url");
+const PDFMerge = require("pdf-merge");
+const path = require("path");
+
+module.exports = async function(result, html, config) {
+
     const parsedConfig = JSON.parse(config);
 
     const htmlFileName = await writeFile(html);
